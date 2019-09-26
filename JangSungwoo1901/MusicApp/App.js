9d35c88bb0717e4d61fab2653925ff0de63f115a
jangsungwoo <jsw7095@gmail.com>
2019-09-27 02:41:55 +0900

    Add TogglePlaybackButton(play or pause) function, Use useState(),TouchableOpacity,Image Components

--- JangSungwoo1901/MusicApp/App.js
+++ JangSungwoo1901/MusicApp/App.js
@@ -6,23 +6,43 @@
  * @flow
  */
 
-import React from 'react';
+import React,{useState} from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
-  Text,
-  StatusBar,
+  TouchableOpacity,
+  Image,
 } from 'react-native';
 
-export default function App () {
+export default function App() {
+  //재생/일시정지 상태변수 선언 
+  const [isPlaying,setIsPlaying] = useState(false);
+  //재생/일시정지 버튼
+  function TogglePlaybackButton({ onPress }) {
+    var icon = isPlaying
+    ? require('./src/images/baseline_pause_black_48dp.png')
+    : require('./src/images/baseline_play_arrow_black_48dp.png');
+    return (
+      <TouchableOpacity style={styles.controlButtonContainer} onPress={onPress}>
+       <Image source={icon}></Image>
+      </TouchableOpacity>
+    );
+  }
+
   return (
-    <View>
-      
+    <View style={styles.controls}>
+        <TogglePlaybackButton onPress={()=>setIsPlaying(!isPlaying)}></TogglePlaybackButton>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  controlButtonContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  controls: {
+    flexDirection: 'row',
+    justifyContent: 'center'
+  }
 });
