ee7b6dd4319006f5b9d70b56701a5f7f5fc1b7bc
jangsungwoo <jsw7095@gmail.com>
2019-09-27 02:45:42 +0900

    Add PreviousButton and NextButton

--- JangSungwoo1901/MusicApp/App.js
+++ JangSungwoo1901/MusicApp/App.js
@@ -28,14 +28,44 @@ export default function App() {
       </TouchableOpacity>
     );
   }
+  
 
   return (
     <View style={styles.controls}>
+        <PreviousButton onPress={_skipToPrevious}></PreviousButton>
         <TogglePlaybackButton onPress={()=>setIsPlaying(!isPlaying)}></TogglePlaybackButton>
+        <NextButton onPress={_skipToNext}></NextButton>
     </View>
   );
 };
 
+//이전음악으로 이동버튼
+function PreviousButton({ onPress }) {
+  return (
+    <TouchableOpacity style={styles.controlButtonContainer} onPress={onPress}>
+      <Image source={require('./src/images/baseline_skip_previous_black_48dp.png')}></Image>
+    </TouchableOpacity>
+  );
+}
+
+//다음음악으로 이동버튼
+function NextButton({ onPress }) {
+  return (
+    <TouchableOpacity style={styles.controlButtonContainer} onPress={onPress}>
+      <Image source={require('./src/images/baseline_skip_next_black_48dp.png')}></Image>
+    </TouchableOpacity>
+  );
+}
+
+//이전음악으로 이동
+function _skipToPrevious() {
+  
+}
+
+//다음음악으로 이동
+function _skipToNext() {
+
+}
 const styles = StyleSheet.create({
   controlButtonContainer: {
     flex: 1,
